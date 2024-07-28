8b99ff1d45a968f90652d6123676c3ab5c251efe
v0d1ch <sasha.bogicevic@iohk.io>
2024-07-28 11:58:15 +0200

    Fix telescope mappings (use nix to configure)

--- config/options/options.nix
+++ config/options/options.nix
@@ -53,10 +53,10 @@
 
   globals.mapleader = " ";
 
-  highlight = {
-    Comment.fg = "#ff00ff";
-    Comment.bg = "#000000";
-    Comment.underline = true;
-    Comment.bold = true;
-  };
+  # highlight = {
+  #   Comment.fg = "#ff00ff";
+  #   Comment.bg = "#000000";
+  #   Comment.underline = true;
+  #   Comment.bold = true;
+  # };
 }
