ec2a2fa969e9296402854d08df9626ebf09a3e2e
v0d1ch <sasha.bogicevic@iohk.io>
2024-07-28 14:38:08 +0200

    Add coc-nvim commands to jump to definition etc.

--- config/plugins/coc-nvim.nix
+++ config/plugins/coc-nvim.nix
@@ -5,6 +5,10 @@
   extraConfigVim = '' 
     xmap <leader>a  <Plug>(coc-codeaction-selected)
     nmap <leader>a  <Plug>(coc-codeaction-selected)
+    nmap <Leader>d <Plug>(coc-definition)
+    nmap <Leader>t <Plug>(coc-type-definition)
+    nmap <Leader>gr <Plug>(coc-references)
+    nmap <Leader>gi <Plug>(coc-implementation)
     nmap <Leader>z :CocDiagnostics<CR>
     inoremap <expr> <CR> pumvisible() ? "\<C-Y>" : "\<CR>" 
   ''; 
