fd4d484c9e0ccdc58145352abfaea31cce34179e
v0d1ch <sasha.bogicevic@iohk.io>
2024-08-03 12:49:31 +0200

    Relative numbers

--- config/options/options.nix
+++ config/options/options.nix
@@ -25,7 +25,7 @@
     smarttab = true;
 
     # System clipboard support, needs xclip/wl-clipboard
-    clipboard = "+unnamedplus";
+    clipboard = "unnamedplus";
 
     # Highlight the current line
     cursorline = true;
