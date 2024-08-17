6dbdd00a73ba14978c453045e7849553722d43f7
v0d1ch <sasha.bogicevic@iohk.io>
2024-08-17 22:23:40 +0200

    Add rust-analyzer

--- config/plugins/plugins.nix
+++ config/plugins/plugins.nix
@@ -1,11 +1,16 @@
 {
-  plugins.neogit.enable = true;
-  plugins.coq-nvim.enable = true;
-  plugins.undotree.enable = true;
-  plugins.vimtex.enable = true;
-  plugins.cmp.enable = true;
-  plugins.markdown-preview.enable = true;
-  plugins.illuminate.enable = true;
-  plugins.hardtime.enable = true;
-  plugins.diffview.enable = true;
+    plugins.neogit.enable = true;
+    plugins.coq-nvim.enable = true;
+    plugins.undotree.enable = true;
+    plugins.vimtex.enable = true;
+    plugins.cmp.enable = true;
+    plugins.markdown-preview.enable = true;
+    plugins.illuminate.enable = true;
+    plugins.hardtime.enable = true;
+    plugins.diffview.enable = true;
+    plugins.lsp.servers.rust-analyzer = {
+        enable = true;
+        installCargo = false;
+        installRustc = false;
+    };
 }
