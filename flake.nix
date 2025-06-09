105ba505405981a873f7608d766a6ecbd67c3a30
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-06-09 11:22:10 +0200

    use 25.05 and update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
     ctrlsf = {
