358bdf37f3c66db75b65a2db3cd8115578cce18b
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-12-31 14:05:11 +0100

    Update nixvim

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
     ctrlsf = {
