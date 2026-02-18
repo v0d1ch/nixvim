bd356c84aec8342882922e6a18e5bd516648661b
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2026-02-18 16:19:00 +0100

    Add coc hover without mouse

--- config/plugins/coc-nvim.nix
+++ config/plugins/coc-nvim.nix
@@ -4,14 +4,18 @@
   # nmap <Leader>gr <Plug>(coc-references)
   # nmap <Leader>gi <Plug>(coc-implementation)
   ## here we need to make sure enter works for selecting the suggestion
-  extraConfigVim = '' 
+  extraConfigVim = ''
     let g:coc_data_home = $HOME . '/.config/coc'
     xmap <leader>a  <Plug>(coc-codeaction-selected)
     nmap <leader>a  <Plug>(coc-codeaction-selected)
     nmap <leader>d <Plug>(coc-definition)
     nmap <leader>t <Plug>(coc-type-definition)
     nmap <leader>z :CocDiagnostics<CR>
-    inoremap <expr> <CR> pumvisible() ? "\<C-Y>" : "\<CR>" 
+    " Show hover info (function signature, type, docs)
+    nmap K :call CocActionAsync('doHover')<CR>
+    " Alternative: <leader>h for hover
+    nmap <leader>h :call CocActionAsync('doHover')<CR>
+    inoremap <expr> <CR> pumvisible() ? "\<C-Y>" : "\<CR>"
   ''; 
 
 }
